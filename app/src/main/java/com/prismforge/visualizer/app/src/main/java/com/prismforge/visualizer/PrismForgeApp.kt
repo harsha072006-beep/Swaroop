d86c7d72032671d51e2027d1b080826e7e0abe54
harsha072006-beep <harsha072006@gmail.com>
2026-09-15 21:28:17 +0530

    Add PrismForgeApp composable for main UI

---
+++ app/src/main/java/com/prismforge/visualizer/app/src/main/java/com/prismforge/visualizer/PrismForgeApp.kt
@@ -0,0 +1,27 @@
+package com.prismforge.visualizer
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun PrismForgeApp() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF08090D)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "PRISM FORGE",
+            color = Color.White
+        )
+    }
+}
